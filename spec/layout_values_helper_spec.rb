6cc2d4b0a8e20fbce5760352477e555421ccf886
Sunny Ripert <sunny@sunfox.org>
2015-08-02 13:44:46 +0200

    Specs rewrite to test all methods

--- spec/layout_values_helper_spec.rb
+++ spec/layout_values_helper_spec.rb
@@ -1,70 +1,120 @@
 require_relative "../app/helpers/layout_values_helper"
-
-# required for #present?
-require "active_support/core_ext/string"
+require "spec_helper"
 
 describe LayoutValuesHelper do
-  include LayoutValuesHelper
+  class MockView
+    include LayoutValuesHelper
 
-  let(:controller) { double(:controller, controller_name: "pages", action_name: "index") }
-
-
-  it "should set the title in content_for" do
-    allow(self).to receive(:content_for)
-    title "hi"
-    expect(self).to have_received(:content_for).with(:title, "hi")
-  end
-
-  it "should return the title when it gets set" do
-    allow(self).to receive(:content_for)
-    expect(title("hi")).to eq("hi")
-  end
-
-  describe "with a content_for" do
-    before do
-      allow(self).to receive(:content_for?).with(:title) { true }
-      allow(self).to receive(:content_for).with(:title) { "hi" }
+    def content_for?(*)
+      false
     end
 
-    it "should return the value through the format translation" do
-      allow(self).to receive(:t).with("layout.title.format", title: "hi", default: "hi") { "hi - my site" }
-      allow(self).to receive(:strip_tags).with("hi - my site") { "hi - my site" }
-      expect(title).to eq("hi - my site")
+    def content_for(*)
+      nil
+    end
+
+    def t(*)
+      ""
+    end
+
+    def strip_tags(v)
+      v
     end
   end
 
-  describe "with an empty content_for" do
-    before do
-      allow(self).to receive(:content_for?).with(:title) { true }
-      allow(self).to receive(:content_for).with(:title) { "" }
-    end
+  let(:view) { MockView.new }
+  let(:controller) {
+    double(:controller, controller_name: "pages", action_name: "index")
+  }
+  before { allow(view).to receive(:controller) { controller } }
 
-    it "should return the value through the default translation" do
-      allow(self).to receive(:t).with("layout.title.default", default: "") { "default" }
-      allow(self).to receive(:strip_tags).with("default") { "default" }
-      expect(title).to eq("default")
-    end
-  end
+  methods = %i(title meta_description meta_keywords)
+  methods.each do |method|
+    describe "##{method}" do
+      describe "with an argument" do
+        it "sets the content_for" do
+          allow(view).to receive(:content_for)
+          view.send(method, "bar")
+          expect(view).to have_received(:content_for).with(method, "bar")
+        end
 
-  describe "without a content_for" do
-    before do
-      allow(self).to receive(:content_for?).with(:title) { false }
-    end
+        it "returns the value it sets" do
+          expect(view.send(method, "bar")).to eq("bar")
+        end
+      end
 
-    it "should return the title in the controller translation" do
-      allow(self).to receive(:t).with("pages.index.title", default: "") { "translated" }
-      allow(self).to receive(:t).with("layout.title.format", title: "translated", default: "translated") { "formatted translated" }
-      allow(self).to receive(:strip_tags).with("formatted translated") { "formatted translated" }
+      describe "with no arguments" do
+        describe "without a content_for" do
+          before do
+            allow(view).to receive(:content_for?).with(method) { false }
+          end
 
-      expect(title).to eq("formatted translated")
-    end
+          it "defaults to ''" do
+            expect(view.send(method)).to eq("")
+          end
 
-    it "should return the default if no controller translation" do
-      allow(self).to receive(:t).with("pages.index.title", default: "") { "" }
-      allow(self).to receive(:t).with("layout.title.default", default: "") { "default" }
-      allow(self).to receive(:strip_tags).with("default") { "default" }
+          it "returns the controller locale value" do
+            allow(view).to receive(:t).with("pages.index.#{method}",
+                                            default: "") {
+              "translated"
+            }
+            allow(view).to receive(:t).with("layout.#{method}.format",
+                                            method => "translated",
+                                            default: "translated") {
+              "formatted translated"
+            }
 
-      expect(title).to eq("default")
+            expect(view.send(method)).to eq("formatted translated")
+          end
+
+          it "returns the default controller locale value" do
+            allow(view).to receive(:t).with("pages.index.#{method}",
+                                            default: "") { "" }
+            allow(view).to receive(:t).with("layout.#{method}.default",
+                                            default: "") { "default" }
+
+            expect(view.send(method)).to eq("default")
+          end
+        end
+
+        context "with a content_for" do
+          before do
+            allow(view).to receive(:content_for?).with(method) { true }
+            allow(view).to receive(:content_for).with(method) { "foo" }
+          end
+
+          it "returns the content formatted" do
+            allow(view).to receive(:t).with("layout.#{method}.format",
+                                            method => "foo",
+                                            default: "foo") { "foo - my site" }
+
+            expect(view.send(method)).to eq("foo - my site")
+          end
+
+          it "strips tags" do
+            allow(view).to receive(:t).with("layout.#{method}.format",
+                                            method => "foo",
+                                            default: "foo") { "foo - my site" }
+            allow(view).to receive(:strip_tags) { "stripped" }
+
+            expect(view.send(method)).to eq("stripped")
+          end
+        end
+
+        context "with an empty content_for" do
+          before do
+            allow(view).to receive(:content_for?).with(method) { true }
+            allow(view).to receive(:content_for).with(method) { "" }
+          end
+
+          it "returns the locales default" do
+            allow(view).to receive(:t).with("layout.#{method}.default",
+                                            default: "") { "default" }
+
+            expect(view.send(method)).to eq("default")
+          end
+        end
+      end
     end
   end
 end
